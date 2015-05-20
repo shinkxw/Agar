16f50ffbadf488399e1be6361a995591f03ed9a8
shink <shink_7770@qq.com>
2015-05-21 01:22:36 +0800

    Agar显示初步实现

--- 代码/Agar容器.rb
+++ 代码/Agar容器.rb
@@ -4,15 +4,22 @@ class Agar容器
   attr_reader :长, :宽
   def initialize(长, 宽)
     @长, @宽, @Agar数组 = 长, 宽, []
+    @父界面 = TK主界面.新建("Agar", 600, 400)
+    @画布 = TK画布.新建(@父界面, @长, @宽, true)
+    @画布.背景色 = 'green'
   end
   def 添加(agar)
-    agar.x, agar.y = 随机(@长), 随机(@宽)
+    agar.生成精灵(@画布, 随机(@长), 随机(@宽))
     @Agar数组 << agar
   end
   def 数量;@Agar数组.数量 end
-  
-  include Enumerable
-  def each;@Agar数组.each{|agar| yield(Agar)} end
+  def 可视化;K可视化模块.后台运行 end
+  def 运行
+    可视化
+    loop{@Agar数组.每个{|agar| agar.运动}}
+  end  
+  #~ include Enumerable
+  #~ def each;@Agar数组.每个{|agar| yield(Agar)} end
 end
 
 定义测试集 'Agar容器测试' do
