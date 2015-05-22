fd0cb4e740063b729afbca6ef0ef75a523c6020a
shink <shink_7770@qq.com>
2015-05-23 01:30:06 +0800

    实现四方向运动

--- 代码/Agar控制器.rb
+++ 代码/Agar控制器.rb
@@ -3,7 +3,10 @@
 class Agar控制器
   #def initialize;@Agar数组 = [] end
   def 判断移动方向
-    D二维向量.新建(50, 50).单位向量
+    x = (S键盘.RIGHT? ? 1 : 0) + (S键盘.LEFT? ? -1 : 0)
+    y = (S键盘.UP? ? 1 : 0) + (S键盘.DOWN? ? -1 : 0)
+    return nil if (x == 0 && y == 0)
+    D二维向量.新建(x, y).单位向量
   end
   
 end
--- 代码/加载.rb
+++ 代码/加载.rb
@@ -2,7 +2,9 @@
 # encoding: UTF-8
 根路径 = File.dirname(__FILE__)
 
-注册模块('Agar', 根路径, true)
-
-加载模块('可视化')
+忽略错误 do
+  注册模块('Agar', 根路径, true)
+  
+  加载模块('可视化')
+end
 
