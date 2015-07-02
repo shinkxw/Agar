a6e7008b13200b5809bf06d45fd91edb88f74712
shink <shink_7770@qq.com>
2015-07-02 23:45:06 +0800

    帧数回调显示

--- Run.rb
+++ Run.rb
@@ -4,7 +4,7 @@ require_relative '../SCRF/加载'
 require_relative '代码/加载'
 
 忽略错误 do
-  容器 = Agar容器.新建(300, 300)
+  容器 = Agar容器.新建(600, 400)
   玩家 = Agar.新建('Shink')
   玩家2 = Agar.新建('Shink2', 50, 'blue', Agar静止控制器.新建)
   容器.添加(玩家, true)
