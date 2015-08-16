3ce8c52a89b1dcd88dd5176fa1e463c1d7eecf58
shink <shink_7770@qq.com>
2015-08-16 20:39:48 +0800

    使用初始化代替initialize

--- 代码/C细胞控制器.rb
+++ 代码/C细胞控制器.rb
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
 class C细胞控制器
-  #def initialize;@细胞数组 = [] end
+  #def 初始化;@细胞数组 = [] end
   def 判断移动方向
     x = (S键盘.RIGHT? ? 1 : 0) + (S键盘.LEFT? ? -1 : 0)
     y = (S键盘.UP? ? 1 : 0) + (S键盘.DOWN? ? -1 : 0)
