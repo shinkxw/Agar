439354280f009914cd5beadd6e22cb3a676e8e89
shink <shink_7770@qq.com>
2015-09-16 21:09:45 +0800

    不再通过y反转来纠正方向

--- 代码/C细胞控制器.rb
+++ 代码/C细胞控制器.rb
@@ -5,7 +5,7 @@ class C细胞控制器
   #def 初始化;@细胞数组 = [] end
   def 判断移动方向
     x = (S键盘.RIGHT? ? 1 : 0) + (S键盘.LEFT? ? -1 : 0)
-    y = (S键盘.UP? ? 1 : 0) + (S键盘.DOWN? ? -1 : 0)
+    y = (S键盘.UP? ? -1 : 0) + (S键盘.DOWN? ? 1 : 0)
     return nil if (x == 0 && y == 0)
     D二维向量.新建(x, y).单位向量
   end
