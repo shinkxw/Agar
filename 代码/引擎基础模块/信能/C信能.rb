8d2da7aa3b5321312cbfe81e25b5bd9a3f82d963
shink <shink_7770@qq.com>
2016-03-11 00:30:59 +0800

    待增加自我对象场识别和形象信息完善

---
+++ 代码/引擎基础模块/信能/C信能.rb
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class C信能
+  可读属性 :能量, :信息
+  def 初始化(能量, 信息 = nil)
+    @能量, @信息 = 能量, 信息
+  end
+end
---
+++ 代码/引擎基础模块/信能/C来源信息.rb
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class C来源信息
+  可读属性 :向量, :对象, :时间戳
+  def 初始化(向量, 对象, 时间戳)
+    @向量, @对象, @时间戳 = 向量, 对象, 时间戳
+  end
+end
