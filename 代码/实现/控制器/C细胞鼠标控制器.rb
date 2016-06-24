def597f6c73f298f28bda1a2688fac7f1277ca53
kanxinwei <shink_7770@qq.com>
2016-06-24 14:01:35 +0800

    主视角显示不再依赖控制器

--- 代码/实现/控制器/C细胞鼠标控制器.rb
+++ 代码/实现/控制器/C细胞鼠标控制器.rb
@@ -1,13 +1,9 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
 class C细胞鼠标控制器 < C控制器
-  def 初始化
-    @主视角 = C主视角显示.新建
-    super(@主视角)
-  end
   def 判断移动方向(输入)
     super
-    鼠标向量 = @主视角.鼠标向量
+    鼠标向量 = $可视化窗口 ? $可视化窗口.鼠标向量 : nil
     return unless 鼠标向量
     return if 鼠标向量.模 < 3
     速度比例 = 鼠标向量.模 > 100 ? 1.0 : (鼠标向量.模 / 100.0)
